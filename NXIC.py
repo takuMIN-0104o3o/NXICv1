65906b7729b5e0386f828bb9208ddfa691e2e459
Daisuke Sasaki <20591351+sakkuntyo@users.noreply.github.com>
2022-10-07 03:42:56 +0900

    感度修正

--- NXIC.py
+++ NXIC.py
@@ -143,8 +143,8 @@ def get_mouse_input():
 def calc_gyro():
     global gyrox, gyroy, gyroz, x, y, mouse_threshold
     gyrox = 0
-    gyroy = int((y / mouse_threshold) * 57.3 / 0.1)
-    gyroz = int(-((x / mouse_threshold) * 57.3 / 0.1))
+    gyroy = int((y / mouse_threshold) * 57.3 / 0.7)
+    gyroz = int(-((x / mouse_threshold) * 57.3 / 0.5))
 
 def get_mouse_and_calc_gyro():
     while True:
